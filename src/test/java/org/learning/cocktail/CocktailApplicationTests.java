7661f7fd479140eeceeb7eea606f38a276d0c426
MatteoFarinazzo <matteofary@gmail.com>
2024-01-19 15:38:29 +0100

    Initial commit

---
+++ src/test/java/org/learning/cocktail/CocktailApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.cocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
