c4fb8cdb1fea9d11229e12db37065a077c3b3643
ValerioTorto <valerio.tortorelli@gmail.com>
2024-01-19 16:15:05 +0100

    category

---
+++ src/main/java/org/learning/cocktail/Controller/Category.java
@@ -0,0 +1,42 @@
+package org.learning.cocktail.Controller;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull
+    private String name;
+    @OneToMany
+    private List<Category> categories;
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
