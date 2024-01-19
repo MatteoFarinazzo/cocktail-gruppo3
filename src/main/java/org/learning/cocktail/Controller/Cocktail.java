827ef7c8bb742209cb3092e022f881c2e3e25daf
MatteoFarinazzo <matteofary@gmail.com>
2024-01-19 16:07:22 +0100

    created cocktail model

--- src/main/java/org/learning/cocktail/Controller/Cocktail.java
+++ src/main/java/org/learning/cocktail/Controller/Cocktail.java
@@ -1,8 +1,62 @@
 package org.learning.cocktail.Controller;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Entity
+@Table(name = "cocktails")
 public class Cocktail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull
+    private String name;
+    private String description;
+    private String image;
+    @NotNull
+    private BigDecimal price;
 
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
