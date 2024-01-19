de9cdfb02307770ba9354b55a02af3520556a8ad
xgiannix <gianni.demare@hotmail.it>
2024-01-19 16:45:26 +0100

    First Commit

--- src/main/java/org/learning/cocktail/controller/categoryController.java
+++ src/main/java/org/learning/cocktail/controller/categoryController.java
@@ -1,4 +1,35 @@
 package org.learning.cocktail.controller;
 
+import org.learning.cocktail.Model.Category;
+import org.learning.cocktail.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categories")
 public class categoryController {
+
+     @Autowired
+    private CategoryRepository categoryRepository;
+
+     @GetMapping
+    public String index(Model model) {
+         model.addAttribute("categoryList", categoryRepository.findAll());
+         return "categories/";
+     }
+
+     @GetMapping("/create")
+    public String create(Model model) {
+         model.addAttribute("formCategory", new Category());
+         return "categories/";
+     }
+
+
+
+
+
 }
